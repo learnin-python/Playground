8547c8753af7d36eb2c76be10ee159d88265d802
Nathan Mkaya <nathanmkaya@gmail.com>
2021-01-11 15:34:59 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import datetime
+
+while True:
+    city = input("Enter city: ")
+
+    current_time = datetime.datetime.now()
+    hour = current_time.hour - 3
+
+    ## Because of timezone GMT + 3 (Nairobi)
+
+    minute = current_time.minute
+    second = current_time.second
+
+    if city == "Boston":
+        hour = hour - 4
+
+    elif city == "Tokyo":
+        hour = hour + 9
+
+    elif city == "Chicago":
+        hour = hour - 5
+
+    elif city == "Nairobi":
+        hour = hour + 3
+
+    elif city == "Seattle":
+        hour = hour - 7
+
+    if city == "Cairo":
+        hour = hour - 2
+
+    elif city == "Karachi":
+        hour = hour + 5
+
+    elif city == " Shanghai":
+        hour = hour + 8
+
+    elif city == " Kolkata":
+        hour = hour + 5
+        minute = minute + 30
+
+    elif city == "exit":
+
+        break
+
+    else:
+        print(city, "is not added")
+        city = "GMT"
+
+    print(city, str(hour) + ":" + str(minute) + ":" + str(second))
